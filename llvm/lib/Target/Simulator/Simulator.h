fe780a8cb057639056e50e297145df9749bdacca
Ани Арутюнян <arutiunianan@icloud.com>
2026-03-30 23:39:51 +0300

    update simulator instruction info

--- llvm/lib/Target/Simulator/Simulator.h
+++ llvm/lib/Target/Simulator/Simulator.h
@@ -5,13 +5,13 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Target/TargetMachine.h"
 
-#define SIMULATOR_DUMP(Color)                                                  \
+/*#define SIMULATOR_DUMP(Color)                                                  \
   {                                                                            \
     llvm::errs().changeColor(Color)                                            \
         << __func__ << "\n\t\t" << __FILE__ << ":" << __LINE__ << "\n";        \
     llvm::errs().changeColor(llvm::raw_ostream::WHITE);                        \
-  }
-// #define SIMULATOR_DUMP(Color) {}
+  }*/
+#define SIMULATOR_DUMP(Color) {}
 
 #define SIMULATOR_DUMP_RED SIMULATOR_DUMP(llvm::raw_ostream::RED)
 #define SIMULATOR_DUMP_GREEN SIMULATOR_DUMP(llvm::raw_ostream::GREEN)
